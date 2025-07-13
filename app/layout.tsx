001ec118f994c4c460f8f4acfd690086411f1720
Tailonandrade <tailondandrade@gmail.com>
2025-07-13 16:20:02 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-gray-100 text-gray-900">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
