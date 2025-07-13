10603caeb0195ae8cbc0b459cac5ab2f39043a69
Tailonandrade <tailondandrade@gmail.com>
2025-07-13 16:52:24 -0300

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// Essas chaves você já me mandou, então vou usar as reais aqui
+const supabaseUrl = "https://ktqmlgvhiuvwehgjeezo.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0cW1sZ3ZoaXV2d2VoZ2plZXpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0Mjc2NzMsImV4cCI6MjA2ODAwMzY3M30.LRq0uwI24AvEtO7iGBWaSXTrraQPfHFSY1vEyvP7OIY";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
